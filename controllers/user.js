9086efa774bee089dd571898cb2c7e67ae9bb049
deepikacha <deepikachalla9>
2024-12-04 21:03:15 +0530

    already exist

--- controllers/user.js
+++ controllers/user.js
@@ -14,32 +14,40 @@ exports.getUser=(req,res,next)=>{
     .catch((err)=>console.log(err))
    
 }
-exports.postAddUser=(req,res,next)=>{
-    const name=req.body.name;
-    const email=req.body.email;
-    const password=req.body.password;
-   
-    User.create({
-        name:name,
-        email:email,
-       password:password
-    })
-    .then((createdUser) => {
-        console.log('User created:', createdUser);
-        res.status(201).json({ 
-            message: 'User created successfully!',
-            user: createdUser
-        });
-    })
 
-    .catch((err) => {
-        console.error(err);
-        res.status(500).json({ 
-            message: 'An error occurred', 
-            error: err 
+exports.postAddUser = (req, res, next) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Check if the user already exists
+    User.findOne({ where: { email: email } })
+        .then((existingUser) => {
+            if (existingUser) {
+                // User already exists
+                return res.status(409).json({
+                    message: 'User already exists',
+                });
+            }
+
+            // Create a new user
+            return User.create({ name, email, password })
+                .then((createdUser) => {
+                    console.log('User created:', createdUser);
+                    res.status(201).json({
+                        message: 'User created successfully!',
+                        user: createdUser,
+                    });
+                });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json({
+                message: 'An error occurred',
+                error: err,
+            });
         });
-    });
-}
+};
 
 
 
